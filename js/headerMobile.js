f86e99420bbdff6e04d2c05ea9decb32959d792a
VadymBezsmertnyi <vados199368@gmail.com>
2021-07-09 21:07:44 +0300

    Working 70%....

---
+++ js/headerMobile.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const navBtn = document.querySelector('#nav-btn'),
+        logo = document.querySelector('.logo'),
+        logoNavigation = document.querySelector('.logoNavigation'),
+        mainNavigation = document.querySelector('#mainNavigation'),
+        navigation = document.querySelector('#navigation'),
+        body = document.querySelector('body'),
+        headerID = document.querySelector('#mainTextTabs');
+
+    navBtn.addEventListener('click', () => {
+        navBtn.style.display = "none";
+        logo.classList.add('logoJS');
+        logoNavigation.classList.add('logoNavigationJS');
+        mainNavigation.classList.remove('mainNavigation');
+        mainNavigation.classList.add('mainNavigationJS');
+        navigation.style.display = "flex";
+    });
+
+    function hideMunuNavigation(){
+        navBtn.style.display = "";
+        logo.classList.remove('logoJS');
+        logoNavigation.classList.remove('logoNavigationJS');
+        mainNavigation.classList.remove('mainNavigationJS');
+        mainNavigation.classList.add('mainNavigation');
+        navigation.style.display = "";
+    }
+
+    body.addEventListener('click', (e) => {
+        if (e.target === body) {
+            hideMunuNavigation();
+        }
+    });
+
+    headerID.addEventListener('click', (e) => {
+        if (e.target !== headerID) {
+            hideMunuNavigation();
+        }
+    });
+});
---
+++ js/textWithTabs.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btnTextTabs = document.querySelectorAll('#btnTextTabs button'),
+              otherTextH2 = document.querySelectorAll('#otherText h2'),
+              otherTextP = document.querySelectorAll('#otherText p'),
+              img = document.querySelectorAll('#otherTextTabs img'),
+              imgMobile = document.querySelectorAll('#otherText img');
+
+    console.log(otherTextH2);
+    console.log(otherTextP);
+    console.log(img);
+
+    function clearBtn(){
+        btnTextTabs.forEach(btn =>{
+            btn.classList.remove('btnShow');
+        });
+    }
+
+    function hideTextTabs(){
+        otherTextH2.forEach(h2 =>{
+            h2.style.display = 'none';
+        });
+        otherTextP.forEach(p =>{
+            p.style.display = 'none';
+        });
+        img.forEach(im =>{
+            im.style.display = 'none';
+        });
+        imgMobile.forEach(imM =>{
+            imM.style.display = 'none';
+        });
+    }
+
+    function showID(i){
+        otherTextH2[i].style.display = '';
+        otherTextP[i].style.display = '';
+        img[i].style.display = '';
+        imgMobile[i].style.display = '';
+    }
+
+    btnTextTabs.forEach((btn, i) =>{
+        
+
+        btn.addEventListener('click', ()=>{
+            clearBtn();
+            btn.classList.add('btnShow');
+
+            hideTextTabs();
+            showID(i);
+        });
+    });
+});
